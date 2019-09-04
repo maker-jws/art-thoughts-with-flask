75634c0e723b988ffafebe8148b9400041874048
Joshua Smith <ws.joshua@gmail.com>
2019-09-04 14:18:39 -0600

    put and delete routes active

--- api/data.py
+++ api/data.py
@@ -10,6 +10,7 @@ data = Blueprint("data", "data", url_prefix="/data/v1")
 @data.route("/", methods=["GET"])
 def get_query_results():
     try:
+        print(request, 'request on get route-Query')
         queries = [model_to_dict(query) for query in models.Data.select()]
         return jsonify(data=queries, status={"code": 200, "message": "Success"})
     except models.DoesNotExist:
@@ -23,9 +24,35 @@ def get_query_results():
 @data.route('/', methods=["POST"])
 def store_query():
     print(request, "request")
-    payload = request.form.to_dict()
+    payload = request.get_json()
     print(payload)
     query = models.Data.create(**payload)
     # print(query.___dict___, 'inside query')
     query_dict = model_to_dict(query)
     return jsonify(data=query_dict, status={"code": 201, "message": "Success"})
+
+
+@data.route('/<id>', methods=["GET"])
+def get_one_query(id):
+    query = models.Data.get_by_id(id)
+    return jsonify(data=model_to_dict(query), status={"code": 201, "message": "Success"})
+
+
+@data.route('/<id>', methods=["DELETE"])
+def delete_query(id):
+    query = models.Data.delete().where(models.Data.id == id)
+    response = query.execute()
+    print(response)
+    return jsonify(  # not sure why you need to jsonify
+        data="resource successfully deleted",
+        status={"code": 200, "message": "Resource deleted"})
+
+
+@data.route('/<id>', methods=["PUT"])
+def update_query(id):
+    payload = request.get_json()
+    print('payload:', payload)
+    query = models.Data.update(**payload).where(models.Data.id == id)
+    query.execute()
+    updated_query = models.Data.get_by_id(id)
+    return jsonify(data=model_to_dict(updated_query), status={"code": 200, "message": "Success"})
